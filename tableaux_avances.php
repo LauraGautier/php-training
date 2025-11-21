0effa9da600ec853ac4a3c5f7a88d9293bc7d384
Laura Gautier <lc.gautier@icloud.com>
2025-11-21 16:11:39 +0100

    add : (suite) exercices corrigés sur les tableaux associatifs

--- tableaux_avances.php
+++ tableaux_avances.php
@@ -7,4 +7,26 @@ $film = [
     "realisateur" => "Mathieu Kassovitz"
 ];
 
-echo "Le film " . $film["titre"] . " de " . $film["realisateur"] . " est sorti en " . $film["annee"] . ".";
+echo "Le film " . $film["titre"] . " de " . $film["realisateur"] . " est sorti en " . $film["annee"] . ". ";
+
+// crée un tableau $stock représentant un inventaire. Avec une boucle, affiche chaque fruit et sa quantité.
+$stock = [
+    "Pommes" => 50,
+    "Bananes" => 30,
+    "Oranges" => 20
+];
+
+foreach ($stock as $fruit => $quantite) {
+    echo $fruit . " : " . $quantite . " unités - ";
+}
+
+// créé un tableau de 3 personnes, chaque personne a un prénom et un âge. Avec une boucle, affiche : "[Prénom] a [âge] ans."
+$personnes = [
+    ["prenom" => "Marie", "age" => 22],
+    ["prenom" => "Paul", "age" => 24],
+    ["prenom" => "Sophie", "age" => 21]
+];
+
+foreach ($personnes as $personne) {
+    echo $personne["prenom"] . " a " . $personne["age"] . " ans. ";
+}
